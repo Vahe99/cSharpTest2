8d581d413d0d60be6014bdd902ae1e320174e8f1
Vahe99 <61380709+Vahe99@users.noreply.github.com>
2020-09-12 14:50:41 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,103 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+using System.Text;
 
-namespace ConsoleApp1
+namespace MyProject
 {
-    class student
+    class Program
     {
-       
-        public static int count = 0;
-
-        public string name;
-        public int age;
-        public string nation;
-
-        public student()
+        static void Main(string[] args)
         {
+            Console.Write("Введите количество элементов массива:\t");
+            int elementsCount = int.Parse(Console.ReadLine());
 
-        }
-        public student(string _name, int _age, string _nation)
-        {
-            name = _name;
-            age = _age;
-            nation = _nation;
+            int[] myArray = new int[elementsCount];
 
-            print();
-        }
-        
-
-        public void print()
-        {
-            Console.WriteLine("Name - " + name);
-            Console.WriteLine("Age - " + age);
-            Console.WriteLine("nation - " + nation);
-        }
-        
-    }
-    
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            string yourName;
-            Console.WriteLine("What is your name?");
-            yourName = Console.ReadLine();
-            Console.WriteLine("Hello {0} nicw to meet you.", yourName);
-
-            int age;
-            Console.WriteLine("How old are you?");
-            age = Convert.ToInt32(Console.ReadLine());
-
-            if (age <= 18)
+            for (int j = 0; j < myArray.Length; j++)
             {
-                Console.WriteLine("Then you are still a minor.", age);
+                Console.Write($"\nВведите элемент массива под индексом {j}:\t");
+                myArray[j] = int.Parse(Console.ReadLine());
             }
-            else if(age >= 19 && age <= 40)
+
+            Console.WriteLine("\nВывод массива");
+
+            for (int i = 0; i < myArray.Length; i++)
             {
-                Console.WriteLine("You have a whole life ahead", age);
-            }if(age >= 41)
-            {
-                Console.WriteLine("You look younger than {0}.", age);
+                Console.WriteLine(myArray[i]);
             }
-            
-         
 
-            string yourNation;
-            Console.WriteLine("What is you nationality?");
-            yourNation = Console.ReadLine();
-            Console.WriteLine("Wow {0} are a very ancient nation!", yourNation);
+            int syze = myArray.Length;
 
-            
-            Console.WriteLine("Let's check how well you know the history of the {0} people.", yourNation);
 
-            int number_1;
-            Console.WriteLine("1)In what year did Armenia adopt Christianity?");
-            do
+            for (int a = 0; a < syze - 1; a++)
             {
-                number_1 = Convert.ToInt32(Console.ReadLine());
-            } while (number_1 != 301);
-            Console.WriteLine("Well done, {0} correct answer!", number_1);
+                for (int s = (syze - 1); s > a; s--)
+                {
+                    if (myArray[s - 1] > myArray[s])
+                    {
+                        int temp = myArray[s - 1];
+                        myArray[s - 1] = myArray[s];
+                        myArray[s] = temp;
+                    }
+                }
+            }
 
-            int number_2;
-            Console.WriteLine("2)What year was the battle of Avarair?");
-            do
-            {
-                number_2 = Convert.ToInt32(Console.ReadLine());
-            } while (number_2 != 451);
-            Console.WriteLine("Well done, {0} correct answer!", number_2);
 
-            int number_3;
-            Console.WriteLine("3)In what year did Mesrop Mashtots create the Armenian alphabet?");
-            do
-            {
-                number_3 = Convert.ToInt32(Console.ReadLine());
-            } while (number_3 != 405);
-            Console.WriteLine("Well you answered all my questions. Congratulations!", number_3);
-
-            Console.WriteLine();
-            Console.WriteLine("Here are your detals.");
-            student Name = new student(yourName, age, yourNation);
-           
+            Console.ReadKey(); 
         }
     }
 }
+
+
